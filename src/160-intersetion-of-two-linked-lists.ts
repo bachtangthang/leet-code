f22d77e704288a1c19172d77a27e705c7c1b3caa
bach tang thang <thangbt@antsomi.com>
2024-03-27 17:32:25 +0700

    problem 160: intersection of two linked lists

---
+++ src/160-intersetion-of-two-linked-lists.ts
@@ -0,0 +1,18 @@
+import { ListNode, buildList } from './data-structure/linked-list';
+
+function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+  if (headA === null || headB === null) return null;
+  let tempA = headA;
+  while(tempA !== null) {
+    let tempB = headB
+    while(tempB !== null) {
+      if(tempA === tempB) {
+        return tempA;
+      } else {
+        tempB = tempB.next;
+      }
+    }
+    tempA = tempA.next;
+  }
+  return null;
+};
