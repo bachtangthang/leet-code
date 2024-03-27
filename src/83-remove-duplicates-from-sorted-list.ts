668d10780ffa7d856424c195a4d5f25949b040b7
bach tang thang <thangbt@antsomi.com>
2024-03-27 16:11:29 +0700

    problem 83: remove duplaicates from sorted list

--- src/83-remove-duplicates-from-sorted-list.ts
+++ src/83-remove-duplicates-from-sorted-list.ts
@@ -1,5 +1,19 @@
-import { ListNode } from './data-structure/linked-list';
+import { ListNode, buildList } from './data-structure/linked-list';
 
 function deleteDuplicates(head: ListNode | null): ListNode | null {
-  return null;
+  if (head === null) return head;
+  if (head.next !== null) {
+    if (head.val === head.next.val) {
+      head.next = head.next.next;
+      deleteDuplicates(head);
+    } else {
+      head.next = deleteDuplicates(head.next);
+    }
+    return head;
+  }
+  return head;
 };
+
+
+const list = new ListNode();
+list.printList(deleteDuplicates(buildList([1,1,1])));
